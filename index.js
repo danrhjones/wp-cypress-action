4aca6acd262c7b7d6c965eafeff0da0619f147f4
Dan Jones <dan.rh.jones@gmail.com>
2020-05-18 13:19:43 +0100

    update action

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // @ts-check
 import {exec} from '@actions/exec'
 import {which} from '@actions/io'
-import {debug, warning, info, getInput, setFailed} from "@actions/core";
-import {create, UploadOptions} from '@actions/artifact'
-import {Inputs, getDefaultArtifactName} from './constants'
-import {findFilesToUpload} from './search'
+import {debug, getInput, setFailed} from "@actions/core";
+import {Inputs} from './constants'
 
 
 const installDependancies = () => {
@@ -56,44 +54,10 @@ const runTests = () => {
   })
 }
 
-const uploadArtifacts = async () => {
-  try {
-    const name = getInput(Inputs.Name, {required: false})
-    const path = getInput(Inputs.Path, {required: true})
-
-    const searchResult = await findFilesToUpload(path)
-    if (searchResult.filesToUpload.length === 0) {
-      warning(
-          `No files were found for the provided path: ${path}. No artifacts will be uploaded.`
-      )
-    } else {
-      info(
-          `With the provided path, there will be ${searchResult.filesToUpload.length} files uploaded`
-      )
-      debug(`Root artifact directory is ${searchResult.rootDirectory}`)
-
-      const artifactClient = create()
-      const options = {
-        continueOnError: true
-      }
-      await artifactClient.uploadArtifact(
-          name || getDefaultArtifactName(),
-          searchResult.filesToUpload,
-          searchResult.rootDirectory,
-          options
-      )
-
-      core.info('Artifact upload has finished successfully!')
-    }
-  } catch (err) {
-    core.setFailed(err.message)
-  }
-}
 
 installDependancies()
 .then(runWpCypress)
 .then(runTests)
-.then(uploadArtifacts)
 .then(() => {
   debug('all done, exiting')
   // force exit to avoid waiting for child processes,
